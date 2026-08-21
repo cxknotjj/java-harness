6f00c60e6132a24004c3a646f129000b56313f39
cxknotjj <2322055372@qq.com>
2026-08-21 20:51:22 +0800

    java-harness初始化

---
+++ src/main/java/com/dark/javaHarness/JavaHarnessApplication.java
@@ -0,0 +1,13 @@
+package com.dark.javaHarness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaHarnessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaHarnessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dark/javaHarness/JavaHarnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dark.javaHarness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHarnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
